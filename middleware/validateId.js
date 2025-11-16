31d23aa0a130c5153873a64ab393b385f9c94ef0
aliyarerbolatov12-maker <aliyarerbolatov12@gmail.com>
2025-11-16 12:54:52 +0500

    feat(middleware): add validateId middleware for product routes

--- middleware/validateId.js
+++ middleware/validateId.js
@@ -1,15 +1,14 @@
 const getUrlId = require("../helpers/getUrlId");
+const jsonTostringify = require("../helpers/jsonTostringify");
 
-function validateId(req, res, next) {
+module.exports = function validateId(req, res, next) {
   const id = Number(getUrlId(req));
 
-  if (isNaN(id) || id <= 0) {
+  if (!id || isNaN(id) || id <= 0) {
     res.statusCode = 400;
-    return res.end(jsonTostringify({ message: "Invalid product ID" }));
+    return res.end(jsonTostringify({ message: "Invalid ID" }));
   }
 
   req.id = id;
   next();
-}
-
-module.exports = validateId;
+};
